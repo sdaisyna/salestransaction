4fad09548e01f53f2436411db19379e1a5cdc31a
Daisyna Shrestha <sdaisyna335@gmail.com>
2020-09-16 13:41:03 +0545

    Add model login

---
+++ SalesTransaction.Application.Model/Account/MvAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesTransaction.Application.Model.Account
+{
+    class MvAccount
+    {
+    }
+
+    public class MvLogin
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
