d04ffa0fcd164694d6a5cffd836f0d1c7a907957
Daisyna Shrestha <sdaisyna335@gmail.com>
2020-09-17 09:52:46 +0545

    Add Controllers

---
+++ SalesTransaction.Application.WebAPI/Areas/Account/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using SalesTransaction.Application.Model.Account;
+using SalesTransaction.Application.Service.Account;
+using SalesTransaction.Application.WebApi.Areas.Base;
+
+
+namespace SalesTransaction.Application.WebApi.Areas.Account
+{
+    public class AccountController : BaseController
+    {
+        private IAccountService _as;
+
+        public AccountController(IAccountService accountService)
+        {
+            _as = accountService;
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] MvLogin login)
+        {
+            try
+            {
+                dynamic jsonString = _as.GetLogin(login);
+                return Ok(jsonString);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult UserDetail(string json)
+        {
+            try
+            {
+                dynamic jsonString = _as.GetUserDetail(json);
+                return Ok(jsonString);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
+
---
+++ SalesTransaction.Application.WebAPI/Areas/Base/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesTransaction.Application.WebApi.Areas.Base
+{
+    [Produce("application/json")]
+    [EnableCors("AllowOrigin"), Route("api/[controller]/[action]/{id?}")]
+
+    public class BaseController : Controller
+    {
+
+    }
+}
